a57ea9484d2364efe3541d65a26d09fe5ded9ff7
VladimirBabchenko <mgrinevitch@gmail.com>
2017-10-29 23:43:17 +0200

    near

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,24 @@ mainNavButton.addEventListener("click", function(event) {
     mainNav.classList.toggle("main-nav--show");
 });
 
+// Search-block
+
+var search  = document.querySelector(".search");
+var searchBlock = search.getElementsByClassName("search__block")[0];
+search.addEventListener("click", function(event) {
+    event.preventDefault();
+    searchBlock.classList.toggle("search__block--show");
+});
+
+// Bucket-block
+var bucket = document.querySelector(".bucket");
+var bucketBlock = document.querySelector(".bucket__block");
+bucket.addEventListener("click", function(event) {
+    event.preventDefault();
+    bucketBlock.classList.toggle("bucket__block--show");
+});
+
+
+
+
 
