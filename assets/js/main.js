3e2fa79b3f49258728ba39f29faab67f6870d034
Vladimir <mgrinevitch@gmail.com>
2017-10-30 15:56:05 +0200

    next

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,7 @@ search.addEventListener("click", function(event) {
 });
 
 // Bucket-block
+
 var bucket = document.querySelector(".bucket");
 var bucketBlock = document.querySelector(".bucket__block");
 bucket.addEventListener("click", function(event) {
@@ -24,6 +25,37 @@ bucket.addEventListener("click", function(event) {
     bucketBlock.classList.toggle("bucket__block--show");
 });
 
+// Tabs-product
+
+// var tabButton = document.querySelectorAll(".tabs__button-item");
+// var tabContent = document.querySelectorAll(".tabs__content-item");
+
+// for (var i = 0; i < tabButton.length; i++) {
+// 	tabButton[i].addEventListener("click", function(event) {
+// 		event.preventDefault;
+// 		for (var i = 0; i < tabContent; i++) {
+// 			tabContent[i].classList.remove("tabs__content-item--show");
+// 		}
+
+// 		tabContent[i].classList.add("tabs__content-item--show");
+// 	})
+// }
+
+function openDescription (evt, tab) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for ( i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	tablinks = document.getElementsByClassName("tablinks");
+	for ( i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace("active", "");
+	}
+
+	document.getElementById(tab).style.display = "block";
+	evt.currentTarget.className += " active";
+}
 
 
 
